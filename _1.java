3cc8acda8ca6af9001ed9d8cfa883a2052a07912
mdjunedalam <mdjunedalam220@gmail.com>
2025-01-05 18:49:15 +0530

    wrapper class non primitive data

--- _1.java
+++ _1.java
@@ -3,7 +3,7 @@ class _1{
 
     public static void main(String[] args) {
         
-       Double a = 1;
+      // Double a = 1;  --> Error
 
       /* reason :- 
        *          Type mismatch: cannot convert from int to Double
