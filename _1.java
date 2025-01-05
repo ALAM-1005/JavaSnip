75f2eaa00776159b7d9a2c6cd6b16ec0d0c5abdd
mdjunedalam <mdjunedalam220@gmail.com>
2025-01-05 18:44:53 +0530

    program for wrapper class

---
+++ _1.java
@@ -0,0 +1,14 @@
+class _1{
+
+
+    public static void main(String[] args) {
+        
+       Double a = 1;
+
+      /* reason :- 
+       *          Type mismatch: cannot convert from int to Double
+       * 
+       */
+    }
+
+}
